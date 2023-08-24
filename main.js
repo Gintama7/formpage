041f3a6160e6e7edd021d9739806ff56d2b4290e
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-24 12:05:48 +0530

    2nd adding localstorage

---
+++ main.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+let nameInput = document.querySelector('[name="fullname"]');
+let emailInput = document.querySelector('[name="email"]');
+let phoneInput = document.querySelector('[name="phone"]');
+let dateInput = document.querySelector('[name="date"]');
+let timeInput = document.querySelector('[name="time"]');
+
+form.addEventListener('submit',function(e){   
+        e.preventDefault();        
+        form.classList.add('was-validated');
+        localStorage.setItem('name',nameInput.value);
+        localStorage.setItem('email',emailInput.value);
+        localStorage.setItem('phone',phoneInput.value);
+        localStorage.setItem('date',dateInput.value);
+        localStorage.setItem('time',timeInput.value);      
+})
+
